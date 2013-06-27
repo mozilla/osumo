3ebcf85ab0d0d155de569dd09c7f25c4ec172324
Shuhao Wu <shuhao@shuhaowu.com>
2013-06-27 11:32:22 -0700

    All PEP fixes except for 79 character line limit
    There's some refactoring to be done there so I omitted the change.

--- server.py
+++ server.py
@@ -2,16 +2,17 @@ from osumo import app
 
 from settings import *
 
+
 def main():
-  if DEBUG:
-    app.run(debug=True, host="", port=DEPLOY_PORT)
-  else:
-    # Probably needs to be changed depending on the server configurations.
-    from gevent.wsgi import WSGIServer
-    from werkzeug.contrib.fixer import ProxyFix
-    app.wsgi_app = ProxyFix(app.wsgi_app)
-    server = WSGIServer((DEPLOY_IP, DEPLOY_PORT), app)
-    server.serve_forever()
+    if DEBUG:
+        app.run(debug=True, host="", port=DEPLOY_PORT)
+    else:
+        # Probably needs to be changed depending on the server configurations.
+        from gevent.wsgi import WSGIServer
+        from werkzeug.contrib.fixer import ProxyFix
+        app.wsgi_app = ProxyFix(app.wsgi_app)
+        server = WSGIServer((DEPLOY_IP, DEPLOY_PORT), app)
+        server.serve_forever()
 
 if __name__ == '__main__':
-  main()
+    main()
