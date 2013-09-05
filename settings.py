ca364dacac143562ee13f07afce189cf87c118a9
Shuhao Wu <shuhao@shuhaowu.com>
2013-09-05 17:16:47 -0400

    Changed to production

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ DEPLOY_IP = '127.0.0.1'
 DEPLOY_PORT = int(os.environ.get("PORT", 5291))
 
 BASE_URL = 'http://osumo.paas.allizom.org/'
-SUMO_URL = 'http://fakesumo.paas.allizom.org/'
+SUMO_URL = 'https://support.mozilla.org/'
 
 if os.path.isfile('commit.txt'):
   with open('commit.txt') as f:
